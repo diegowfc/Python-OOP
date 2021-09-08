78ff55a6069869e1a51efd115cd8a0206d30a789
diegowfc <69658917+diegowfc@users.noreply.github.com>
2021-09-08 18:08:33 -0300

    Update corpos.py

--- corpos.py
+++ corpos.py
@@ -3,3 +3,7 @@ class Corpos:
     def __init__(self, massa, coordenada):
       self.massa = massa;
       self.coordenada = coordenada;
+
+    def media(conta1, conta2):
+      media = (conta1.massa + conta2.massa) / 2;
+      print("A média é ", media);
