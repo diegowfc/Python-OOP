3d85e05d1fba59431382b365601300aef7ae6d0d
diegowfc <69658917+diegowfc@users.noreply.github.com>
2021-09-08 15:05:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from corpos import Corpos
 
-corpo_celeste1 = Corpos()
+print("*******************")
+print("Bem vindo!")
+print("*******************")
+
+massa = input('\nDigite a massa do corpo celeste: ');
+corpo_celeste1 = Corpos(massa, 27)
+
+print("Massa do corpo 1:", corpo_celeste1.massa)
