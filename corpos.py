8ae19c5eacb72c855c8c1d290dada1b238385f3a
diegowfc <69658917+diegowfc@users.noreply.github.com>
2021-11-07 00:04:03 -0300

    Update corpos.py

--- corpos.py
+++ corpos.py
@@ -1,9 +1,41 @@
-class Corpos:
+from classes import Planeta, Estrela, Lua, Asteroide
 
-    def __init__(self, massa, coordenada):
-      self.massa = massa;
-      self.coordenada = coordenada;
+print("*******************")
+print("Bem vindo!")
+print("*******************")
 
-    def media(conta1, conta2):
-      media = (conta1.massa + conta2.massa) / 2;
-      print("A média é ", media);
+lista_corpos = []
+
+resposta = True
+while resposta:
+    print("\n-----------------------------------------"
+          "\n->1.Inserir um corpo\n"
+          "->2.Calcular média da massa dos corpos\n"
+          "->3.Calcular o desvio padrão das massas\n"
+          "->4.Calcular a distância média dos corpos\n"
+          "->5.Calcular força gravitacional entre os corpos\n"
+          "->6.Listar corpos por categoria\n"
+          "->7 Mover corpos\n"
+          "->8.Sair\n"
+          "-----------------------------------------")
+    resposta = input("->Escolha uma opção: ")
+    if resposta == "1":
+      n = int(input("->Digite a quantidade de corpos há ser inserido: "))
+      for i in range(0, n):
+        categoria = input("\n->Categoria do corpo:\n1 para Planeta\n2 para Estrela\n3 para Lua\n4 para Asteroide\nEscolha uma opção: ")
+        nome = input(("\n->Insira o nome do corpo: "))
+        massa = float(input("->Insira a massa do corpo: "))
+        x = float(input("->Insira a coordenada x: "))
+        y = float(input("->Insira a coordenada y: "))
+        z = float(input("->Insira a coordenada z: "))
+        if categoria == "1":
+          lista_corpos.append(Planeta(nome, massa, x, y, z))
+        elif categoria == "2":
+          lista_corpos.append(Estrela(nome, massa, x, y, z))
+        elif categoria == "3":
+          lista_corpos.append(Lua(nome, massa, x, y, z))
+        elif categoria == "4":
+          lista_corpos.append(Asteroide(nome, massa, x, y, z))
+
+    for corpo in lista_corpos:
+      print(corpo.__dict__)
