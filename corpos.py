64bfd67fb740743895d31c940c7b0f40c121d961
diegowfc <69658917+diegowfc@users.noreply.github.com>
2021-11-08 13:59:20 -0300

    Update corpos.py

--- corpos.py
+++ corpos.py
@@ -1,42 +1,47 @@
 class Corpos:
-    def __init__(self, nome, massa, x, y, z ):
-      self.nome = nome
-      self.categoria = ""
-      self.massa = massa
-      self.x = x
-      self.y = y
-      self.z = z
+    def __init__(self, nome, massa, x, y, z):
+        self.nome = nome
+        self.categoria = ""
+        self.massa = massa
+        self.x = x
+        self.y = y
+        self.z = z
     lista_corpos = []
 
     def adiciona_corpos(self):
-      Corpos.lista_corpos.append(self)
+        Corpos.lista_corpos.append(self)
 
     @staticmethod
-    def ordena_lista(atributo):
-      lista_ordenada = sorted(Corpos.lista_corpos, key=lambda x: x.__dict__[atributo], reverse=True)
-      for corpo in lista_ordenada:
-        print (f"\n{corpo.__dict__}")
-    
-    @staticmethod
-    def mostra_aviso():
-      print("\nAtenção!\n" "\nInsira ao menos um corpo!\n")
+    def ordena_lista(lista, atributo):
+        lista_ordenada = sorted(lista, key=lambda x: x.__dict__[atributo], reverse=True)
+        for corpo in lista_ordenada:
+            print(f"\n{corpo.__dict__}")
+        return
 
+    @staticmethod
+    def calcula_media(lista, atributo):
+        soma = 0
+        for att in lista:
+            soma += att.__dict__[atributo]
+        resultado_media = soma / len(lista)
+        return resultado_media
+        
 class Planeta(Corpos):
-  def __init__(self, nome, massa, x, y, z ):
-    super().__init__(nome, massa, x, y, z)
-    self.categoria = "planeta"
+    def __init__(self, nome, massa, x, y, z):
+        super().__init__(nome, massa, x, y, z)
+        self.categoria = "planeta"
 
 class Estrela(Corpos):
-  def __init__(self, nome, massa, x, y, z ):
-    super().__init__(nome, massa, x, y, z)
-    self.categoria = "estrela"
+    def __init__(self, nome, massa, x, y, z):
+        super().__init__(nome, massa, x, y, z)
+        self.categoria = "estrela"
 
 class Lua(Corpos):
-  def __init__(self, nome, massa, x, y, z ):
-    super().__init__(nome, massa, x, y, z)
-    self.categoria = "lua"
+    def __init__(self, nome, massa, x, y, z):
+        super().__init__(nome, massa, x, y, z)
+        self.categoria = "lua"
 
 class Asteroide(Corpos):
-  def __init__(self, nome, massa, x, y, z ):
-    super().__init__(nome, massa, x, y, z)
-    self.categoria = "asteroide"
+    def __init__(self, nome, massa, x, y, z):
+        super().__init__(nome, massa, x, y, z)
+        self.categoria = "asteroide"
