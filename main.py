3b203dae77ef8cfd154a126318cbb8ea0a059219
diegowfc <69658917+diegowfc@users.noreply.github.com>
2021-11-07 22:44:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,24 +21,27 @@ while resposta:
       n = int(input("->Digite a quantidade de corpos há ser inserido: "))
       for i in range(0, n):
         categoria = input("\n->Categoria do corpo:\n1 para Planeta\n2 para Estrela\n3 para Lua\n4 para Asteroide\nEscolha uma opção: ")
-        nome = input(("\n->Insira o nome do corpo: "))
-        massa = float(input("->Insira a massa do corpo: "))
-        x = float(input("->Insira a coordenada x: "))
-        y = float(input("->Insira a coordenada y: "))
-        z = float(input("->Insira a coordenada z: "))
-        print(f"\nCorpo com o nome {nome} inserido!\nPreencha as informações do novo corpo: ");
-        if categoria == "1":
-          corpo_planeta = Planeta(nome, massa, x, y, z)
-          Corpos.adiciona_corpos(corpo_planeta)
-        elif categoria == "2":
-          corpo_estrela = Estrela(nome, massa, x, y, z)
-          Corpos.adiciona_corpos(corpo_estrela)
-        elif categoria == "3":
-          corpo_lua = Lua(nome, massa, x, y, z)
-          Corpos.adiciona_corpos(corpo_lua)
-        elif categoria == "4":
-          corpo_asteroide = Asteroide(nome, massa, x, y, z)
-          Corpos.adiciona_corpos(corpo_asteroide)
+        if (categoria == '1' or categoria == '2' or categoria == '3' or categoria == '4'):
+          nome = input(("\n->Insira o nome do corpo: "))
+          massa = float(input("->Insira a massa do corpo: "))
+          x = float(input("->Insira a coordenada x: "))
+          y = float(input("->Insira a coordenada y: "))
+          z = float(input("->Insira a coordenada z: "))
+          print(f"\nCorpo com o nome {nome} inserido!\nPreencha as informações do novo corpo: ");
+          if categoria == "1":
+            corpo_planeta = Planeta(nome, massa, x, y, z)
+            Corpos.adiciona_corpos(corpo_planeta)
+          elif categoria == "2":
+            corpo_estrela = Estrela(nome, massa, x, y, z)
+            Corpos.adiciona_corpos(corpo_estrela)
+          elif categoria == "3":
+            corpo_lua = Lua(nome, massa, x, y, z)
+            Corpos.adiciona_corpos(corpo_lua)
+          elif categoria == "4":
+            corpo_asteroide = Asteroide(nome, massa, x, y, z)
+            Corpos.adiciona_corpos(corpo_asteroide)
+        else:
+          print("\nAtenção!\nEscolha uma opção válida!")
     if resposta == "6":
       if (len(Corpos.lista_corpos) != 0):
         Corpos.ordena_lista('massa')
