898d1b7f8a935239f22df11e3c3232a62a5c8318
diegowfc <69658917+diegowfc@users.noreply.github.com>
2021-11-11 13:53:56 -0300

    Update PossivelMenu.py

--- PossivelMenu.py
+++ PossivelMenu.py
@@ -5,7 +5,7 @@ class Menu(Corpos):
         self.opcoes = {
             "1": self.insere_corpos,
             "2": self.exibe_media,
-            #"3": self.exibe_desvio,
+            "3": self.exibe_desvio,
             #"4": self.exibe_distancia,
             #"5": self.exibe_forca
             "6": self.exibe_lista
@@ -41,36 +41,36 @@ class Menu(Corpos):
         
     @staticmethod
     def insere_corpos():
+        categorias = {'1': Planeta('', 0.0, 0.0, 0.0, 0.0), '2': Estrela('', 0.0, 0.0, 0.0, 0.0), 
+        '3': Lua('', 0.0, 0.0, 0.0, 0.0), '4': Asteroide('', 0.0, 0.0, 0.0, 0.0)}
         n = int(input("->Digite a quantidade de corpos há ser inserido: "))
         for i in range(0, n):
             categoria = input("\n->Categoria do corpo:\n1 para Planeta\n2 para Estrela\n3 para Lua\n4 para Asteroide"
             "\nEscolha uma opção: ")
             if (categoria == '1' or categoria == '2' or categoria == '3' or categoria == '4'):
-                if categoria == "1":
-                    corpo_celeste = Planeta('', 0.0, 0.0, 0.0, 0.0)
-                    corpo_celeste.insere_cadastro()
-                elif categoria == "2":
-                    corpo_celeste = Estrela('', 0.0, 0.0, 0.0, 0.0)
-                    corpo_celeste.insere_cadastro()
-                elif categoria == "3":
-                    corpo_celeste = Lua('', 0.0, 0.0, 0.0, 0.0)
-                    corpo_celeste.insere_cadastro()
-                elif categoria == "4":
-                    corpo_celeste = Asteroide('', 0.0, 0.0, 0.0, 0.0)
-                    corpo_celeste.insere_cadastro()
+                corpo_celeste = categorias[categoria]
+                corpo_celeste.insere_cadastro()
             else:
                 print("\nAtenção!\nEscolha uma opção válida!")
     
     @staticmethod
     def exibe_media():
       if(len(Corpos.lista_corpos) != 0):
-          print(f"\nA média das massas é {Corpos.calcula_media(Corpos.lista_corpos, 'massa')}")
+          print(f"\nA média das massas é {Corpos.calcula_media(Corpos.lista_corpos, '_massa')}")
       else:
           Menu.mostra_aviso()
 
     @staticmethod
     def exibe_lista():
         if (len(Corpos.lista_corpos) != 0):
-            Corpos.ordena_lista(Corpos.lista_corpos, 'massa')
+            Corpos.ordena_lista(Corpos.lista_corpos, '_massa')
+        else:
+            Menu.mostra_aviso()
+
+    @staticmethod
+    def exibe_desvio():
+        if(len(Corpos.lista_corpos) != 0):
+            media = Corpos.calcula_media(Corpos.lista_corpos, '_massa')
+            print("\nO desvio padrão das massas é", Corpos.calcula_desvio(media, Corpos.lista_corpos, '_massa'))
         else:
             Menu.mostra_aviso()
